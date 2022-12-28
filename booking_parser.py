7df4b6db063e8a98dcf07c66358873b47a77d52c
michael kolomenkin <michael.kolomenkin@gmail.com>
2022-12-28 10:21:07 +0200

    Final version - can download and filter booking data

--- booking_parser.py
+++ booking_parser.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
+import pickle
 
 def get_description(html_str: str):
     # Find the relevant part
@@ -58,6 +59,71 @@ def get_reviews(hotel_url):
 
     return all_reviews
 
+def filter_facilities(facilities):
+    facilities = [f for f in facilities.split('\n') if f != '']
+    return facilities
+
+def filter_description(description):
+    description = [f for f in description.split('\n') if f != '' and 'Genius' not in f]
+    return description
+
+def month_in_sent(sent):
+    if 'January' in sent or 'February' in sent or 'March' in sent or 'April' in sent or 'May' in sent or 'June' in \
+            sent or 'July' in sent or 'August' in sent or 'September' in sent or 'October' in sent or 'November' in \
+            sent or 'December' in sent:
+        return True
+    return False
+
+def filter_reviews(all_reviews):
+
+    saved_reviews = []
+    for review_list in all_reviews:
+        reviews = [r for r in review_list.split('\n') if r != '']
+        # Go over everything and find the start and the end of every review
+        state = 'init'
+        where_from = ''
+        who = ''
+        summary = ''
+        grade = 0
+        liked = ''
+        disliked = ''
+        when = ''
+        for k, sent in enumerate(reviews):
+            if state == 'init':
+                if month_in_sent(sent) and 'Reviewed' in sent:
+                    state = 'start'
+                    step_back = 0
+                    if 'Reviewers' in reviews[k - 1]:
+                        step_back = 1
+                    where_from = reviews[k - 5 - step_back]
+                    summary = reviews[k + 1]
+                    who = reviews[k - 1 - step_back]
+                    when = sent.replace('Reviewed: ', '')
+            elif state == 'start':
+                if month_in_sent(sent) and 'Reviewed' in sent:
+                    saved_reviews.append([when, who, where_from, summary, grade, liked, disliked])
+                    step_back = 0
+                    if 'Reviewers' in reviews[k - 1]:
+                        step_back = 1
+                    where_from = reviews[k - 5 - step_back]
+                    summary = reviews[k + 1]
+                    who = reviews[k - 1 - step_back]
+                    when = sent.replace('Reviewed: ', '')
+                elif sent.replace(' ', '').isnumeric():
+                    grade = int(sent.replace(' ', ''))
+                elif 'Liked \xa0·\xa0' in sent:
+                    liked = sent.replace('Liked \xa0·\xa0', '')
+                elif 'Disliked \xa0·\xa0' in sent:
+                    disliked = sent.replace('Disliked \xa0·\xa0', '')
+
+        if len(saved_reviews) > 0:
+            if saved_reviews[-1][0] != when and saved_reviews[-1][1] != who and saved_reviews[-1][2] != where_from and \
+                    saved_reviews[-1][3] != summary and saved_reviews[-1][4] != grade and saved_reviews[-1][5] != liked and \
+                    saved_reviews[-1][6] != disliked:
+                saved_reviews.append([when, who, where_from, summary, grade, liked, disliked])
+    return saved_reviews
+
+
 def parse_hotel_by_url(hotel_url):
     """
     :param hotel_url: the url of a hotel in booking.com
@@ -72,15 +138,34 @@ def parse_hotel_by_url(hotel_url):
         response = urllib.request.urlopen(request)
     except:
         print("URL is not working")
+        return None, None, None
 
     html_bytes = response.read()
     html_str = html_bytes.decode("utf8")
     description, facilities = get_description(html_str)
 
     # Get the reviews
-    get_reviews(hotel_url)
+    all_reviews = get_reviews(hotel_url)
+
+    # with open('/Users/michaelko/Data/askforhotel/example.pkl', 'wb') as dump_f:
+    #     pickle.dump([description, facilities, all_reviews], dump_f)
 
     # req = requests.get(url2, verify=False, params={'cc1': 'us', 'dist': '1', 'pagename': 'park-lane-new-york'})
     # fp = urllib.request.urlopen(hotel_url)
 
+    facilities = filter_facilities(facilities)
+    description = filter_description(description)
+    all_reviews = filter_reviews(all_reviews)
+
+    return description, facilities, all_reviews
+
+if __name__ == '__main__':
+    print('Start filtering')
+
+    with open('/Users/michaelko/Data/askforhotel/example.pkl', 'rb') as dump_f:
+        description, facilities, all_reviews = pickle.load(dump_f)
+
+    facilities = filter_facilities(facilities)
+    description = filter_description(description)
+    all_reviews = filter_reviews(all_reviews)
     pass
--- prepare_data.py
+++ prepare_data.py
@@ -1,11 +1,47 @@
 from booking_parser import parse_hotel_by_url
-
+import pickle
 
 def prepare_data():
-    hotel_url = 'https://www.booking.com/hotel/us/park-lane-new-york.en-gb.html'
+    # hotel_url = 'https://www.booking.com/hotel/us/park-lane-new-york.en-gb.html'
     # hotel_url = 'https://www.booking.com/reviewlist.html?cc1=us;dist=1;pagename=park-lane-new-york;type=total&&offset=1;rows=2'
-    parse_hotel_by_url(hotel_url)
+    # parse_hotel_by_url(hotel_url)
+
+
+    full_data = []
+
+    file_hotels = open('/Users/michaelko/Desktop/hotelstxt.txt', 'r')
+    hotel_names = file_hotels.readlines()
+    for k, hotel_url in enumerate(hotel_names):
+        if k <= 375:
+            continue
+        if 'searchresults' in hotel_url:
+            continue
+        description, facilities, all_reviews = parse_hotel_by_url(hotel_url[hotel_url.find('http'):].replace('\n', ''))
+        if description is not None:
+            full_data.append([description, facilities, all_reviews, hotel_url])
+        if k % 25 == 0 and k > 0:
+            print('Running hotel ', k)
+            with open('/Users/michaelko/Data/askforhotel/new_york_375.pkl', 'wb') as dump_f:
+                pickle.dump(full_data, dump_f)
+
+    with open('/Users/michaelko/Data/askforhotel/new_york_final.pkl', 'wb') as dump_f:
+        pickle.dump(full_data, dump_f)
+
+def unite_several_files():
+    with open('/Users/michaelko/Data/askforhotel/new_york.pkl', 'rb') as dump_f:
+        data1 = pickle.load(dump_f)
+
+    with open('/Users/michaelko/Data/askforhotel/new_york_final.pkl', 'rb') as dump_f1:
+        data2 = pickle.load(dump_f1)
+
+    pass
+
+    data1 = data1 + data2
+
+    with open('/Users/michaelko/Data/askforhotel/new_york_last.pkl', 'wb') as dump_f2:
+        pickle.dump(data1, dump_f2)
 
 if __name__ == '__main__':
     print('Start processing')
+    unite_several_files()
     prepare_data()
